a3d3ccc993df65c7c3a829668cc6d8a655a04556
Michael E Miles <michael@miles-me.net>
2017-05-23 14:19:24 -0500

    Initial Commit

--- PyPigLatin/PyPigLatin.py
+++ PyPigLatin/PyPigLatin.py
@@ -1,1 +1,25 @@
+original = input("Please enter a sentence: ").strip().lower()
+words = original.split()
 
+newWords = []
+vowels = "aeiou"
+
+for word in words:
+    if word[0] in vowels:
+        newWords.append(word + "yay")
+
+    else:
+        vowelPosition = 0
+        for letter in word:
+            if letter not in vowels:
+                vowelPosition = vowelPosition + 1
+
+            else:
+                break
+
+        consonants = word[:vowelPosition]
+        newWords.append(word[vowelPosition:] + consonants + "ay")
+
+output = " ".join(newWords)
+
+print(output)
